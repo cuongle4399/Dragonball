376afef668b83d05cca214d3578682365b8f44ab
Cường Lê <161845711+cuongle4399@users.noreply.github.com>
2025-03-12 19:41:32 +0700

    Mod cong dong 244 cuong le

---
+++ Convert/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    public static void Base64toImage(string s, string filePath)
+    {
+        byte[] imageBytes = Convert.FromBase64String(s);
+        File.WriteAllBytes(filePath, imageBytes);
+        Console.WriteLine("Đã convert base64 to image: Thành Công");
+    }
+    public static void ImageToBase64(string filePath)
+    {
+        byte[] imageBytes = File.ReadAllBytes(filePath);
+        
+        string s = Convert.ToBase64String(imageBytes);
+        File.WriteAllText("output.txt", s);
+        Console.WriteLine("Đã convert image to base64: Thành Công");
+    }
+    static void Main()
+    {
+        ImageToBase64("logo9.png");
+    }
+}
